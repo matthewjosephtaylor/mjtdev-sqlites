b0e254da63e396f0dd4da6fc73b1c9d2e113e0df
Matt Taylor <matt@matthewjosephtaylor.com>
2022-08-28 17:36:52 -0500

    WIP

--- src/unlockDatabase.ts
+++ src/unlockDatabase.ts
@@ -1,7 +1,6 @@
 import { Database } from "sqlite3";
 import { runSql } from "./runSql";
 
-
 export const unlockDatabase = (db: Database, key: string) => {
-  return runSql(db, "PRAGMA key = `${key}`");
+  return runSql(db, `PRAGMA key = '${key}'`);
 };
